9138f6144d4b62c22a4cd93110e83e57f84ccc5f
BrunoReis-NZ <brunoreis.nz@gmail.com>
2024-07-31 07:02:52 +1200

    Specflow framework implemented and Feature file created.

---
+++ TAProgramme/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace TAProgramme.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged in to TurUp portal successfully")]
+        public void GivenILoggedInToTurUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I Navigate to time and Material Page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I create a time record")]
+        public void WhenICreateATimeRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The time record should be created successfully")]
+        public void ThenTheTimeRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
